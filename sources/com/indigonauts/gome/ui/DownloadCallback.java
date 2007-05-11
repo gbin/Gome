9a86f588246028ab641403f3631c476f1a9dfada
gbin <gbin@ab0d379b-fd29-0410-b982-9e38e41d62e4>
2007-05-11 16:21:38 +0000

    put "online" for the go rules ladels
    git-svn-id: svn+ssh://pal.gootz.net/var/svn/repos/gome@89 ab0d379b-fd29-0410-b982-9e38e41d62e4

--- sources/com/indigonauts/gome/ui/DownloadCallback.java
+++ sources/com/indigonauts/gome/ui/DownloadCallback.java
@@ -2,8 +2,6 @@ package com.indigonauts.gome.ui;
 
 import java.util.Vector;
 
-import com.indigonauts.gome.sgf.SgfModel;
-
 public interface DownloadCallback {
   void downloadFinished(String path, Vector files);
   void downloadFailure(Exception reason);
